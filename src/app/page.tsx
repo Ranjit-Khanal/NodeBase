36a9188c7f9c99d72019c6e2d0fed6d1ab0cc5dc
Ranjit-Khanal <ranjitkhanal61@gmail.com>
2025-10-16 10:08:16 +0545

    chore: add all shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
+      <Button variant="ghost">
+
       hello world
+
+      </Button>
     </div>
   );
 }
