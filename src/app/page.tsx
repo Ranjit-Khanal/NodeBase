3abfdec47b3649720d6cce25e5fa4b308480ef2c
Ranjit-Khanal <ranjitkhanal61@gmail.com>
2025-10-18 19:03:35 +0545

    feat: implement ingest background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils";
+import { useTRPC } from "@/trpc/client";
 import { caller } from "@/trpc/server";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-export default async function Home() {
-  await requireAuth();
+export default function Home() {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const data = await caller.getUsers();
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    }),
+  );
+
   return (
-    <div>
+    <div className="flex min-h-screen justify-center items-center flex-col">
       protected page here
-      {JSON.stringify(data)}
+      <p className="flex flex-col">{JSON.stringify(data)}</p>
+      <Button
+        disabled={create.isPending}
+        onClick={() => {
+          console.log("hello");
+          create.mutate();
+        }}
+      >
+        Create workflow
+      </Button>
     </div>
   );
 }
